2edc3d091d4f6e593db8224eafa35856b5989773
Aphiwit1 <Aphiwit.boon@gmail.com>
2017-08-01 15:35:58 +0700

    update add everything

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -21,4 +21,20 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+})
+
+.config(function($stateProvider, $urlRouterProvider){
+
+    $stateProvider.state('master', {
+      url: '/home',
+      templateUrl: 'views/master.html',controller:'MasterController'
+
+    })
+
+    $stateProvider.state('detail',{
+      url: '/detail',
+      templateUrl: 'views/Detail.html'
+    })
+
+    $urlRouterProvider.otherwise('/home');
 })
---
+++ www/js/controller/MasterController.js
@@ -0,0 +1,7 @@
+app.controller('MasterController', ['$scope', function($scope){
+	$scope.news = [
+   {title:'A' , imgUrl:'' , createdDate:new Date() , content:'blah blah blah'},
+   {title:'B' , imgUrl:'' , createdDate:new Date() , content:'blah blah blah'},
+   {title:'C' , imgUrl:'' , createdDate:new Date() , content:'blah blah blah'},
+	];
+}])
